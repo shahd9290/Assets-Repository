a29f9de2681a589fcf84e521f8e414bf584b8e93
Omar <zlac353@live.rhul.ac.uk>
2024-01-29 20:11:41 +0000

    Add POST end point for creating a type table

--- backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/controller/TypeController.java
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/controller/TypeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/type")
@@ -24,6 +25,44 @@ public class TypeController {
         if (tableData == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return ResponseEntity.ok(tableData);
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addType(@RequestBody Map<String, Object> payload) {
+        if (!checkDataValid(payload))
+            return ResponseEntity.badRequest().body("Invalid data provided!");
+
+        String tableName = (String) payload.get("table_name");
+
+        Object columnsObject = payload.get("columns");
+        if (!(columnsObject instanceof List)) {
+            return ResponseEntity.badRequest().body("Invalid 'columns' format. Expected a List<String>.");
+        }
+
+        List<String> columns = (List<String>) columnsObject;
+
+        System.out.println(payload.get("columns").getClass());
+        service.createTable(tableName, columns);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Data added successfully");
+    }
+
+
+    /**
+     * Method to verify the structure of the data received matches the expected one from the API.
+     * @param payload The data received
+     * @return {@code true} if the data matches the requirements, else {@code false}
+     */
+    private boolean checkDataValid(Map<String, Object> payload) {
+
+        if (!payload.containsKey("table_name"))
+            return false;
+
+        if (!payload.containsKey("columns"))
+            return false;
+
+        return true;
     }
 
 }
