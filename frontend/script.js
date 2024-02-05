df783634f0805464c5078d7f533342925a7173ca
Owen Mashingaidze <zkac157@live.rhul.ac.uk>
2024-02-05 11:35:11 +0000

    initial files for frontend app + webpages for creating assets and types

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-function deleteAsset(assetId) {
-    console.log("Deleting asset with ID: " + assetId);
-    // Add server-side interaction code here
-}
-
-function deleteType(typeId) {
-    console.log("Deleting type with ID: " + typeId);
-    // Add server-side interaction code here
-}
