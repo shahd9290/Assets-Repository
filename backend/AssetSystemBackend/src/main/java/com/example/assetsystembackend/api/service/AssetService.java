693db9442254a4d30822f32982b999805ac32132
Omar <zlac353@live.rhul.ac.uk>
2024-02-13 13:54:04 +0000

    Create Asset service for adding assets

---
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/service/AssetService.java
@@ -0,0 +1,34 @@
+package com.example.assetsystembackend.api.service;
+
+import com.example.assetsystembackend.api.model.Asset;
+import com.example.assetsystembackend.api.repository.AssetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AssetService {
+
+    private final AssetRepository assetsRepo;
+
+    @Autowired
+    public AssetService(AssetRepository assetsRepo) {
+        this.assetsRepo = assetsRepo;
+    }
+
+    public List<Asset> getAllAssets() {
+        return assetsRepo.findAll();
+    }
+
+    public Optional<Asset> findByID(Long id){
+        return assetsRepo.findById(id);
+    }
+
+    public Asset saveNewAsset(Asset asset){
+        return assetsRepo.save(asset);
+    }
+
+
+}
