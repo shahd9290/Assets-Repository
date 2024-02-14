f5f206e555ed2902dceb7204bebdd6dd8bec2684
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-02-14 22:26:59 +0000

    Added Type column to assets table

--- backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/model/Asset.java
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/model/Asset.java
@@ -27,12 +27,16 @@ public class Asset {
     @Column(name = "description")
     private String description;
 
-    public Asset(Long id, String name, String creatorName, Date creationDate, String description) {
+    @Column(name = "type")
+    private String type;
+
+    public Asset(Long id, String name, String creatorName, Date creationDate, String description, String type) {
         this.id = id;
         this.name = name;
         this.creatorName = creatorName;
         this.creationDate = creationDate;
         this.description = description;
+        this.type = type;
     }
 
     public Asset() { //Constructor required
@@ -75,10 +79,15 @@ public class Asset {
         this.description = description;
     }
 
+    public String getType() {return type;}
+
+    public void setType(String type) {this.type = type;}
+
+
     @Override
     public String toString(){
-        String output= "{id(%d) | name(%s) | creation date(%s) | description(%s)}";
-        return String.format(output, id, name, creationDate.toString(), description);
+        String output= "{id(%d) | name(%s) | type(%s) | creation date(%s) | description(%s)}";
+        return String.format(output, id, name, type, creationDate.toString(), description);
     }
 
 }
