07902cd412f5b29f17dacc33b4e5f924a7c01d98
Palny Shungu <ZLAC276@live.rhul.ac.uk>
2024-02-19 12:50:00 +0000

    Adding registering user form to homepage

--- frontend/frontend-app/src/Home.js
+++ frontend/frontend-app/src/Home.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Types from "./Types";
 import App from "./App";
+import NewUser from "./NewUser";
 import AssetTable from "./components/AssetTable";
 function Home() {
 
@@ -9,12 +10,13 @@ function Home() {
   return (
     <div>
         <Navbar />
-        <Routes>         
+        <Routes>
             <Route path='/create-assets' element={<App/>} />
             <Route path='/create-types' element={<Types/>} />
             <Route path='/assets' element={<AssetTable/>} />
+            <Route path='/register-user' element={<NewUser/>} />
         </Routes>
-        
+
     </div>
   );
 }
