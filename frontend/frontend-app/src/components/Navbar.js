1d9c333e9de06b7e9dce003a81d3525c34325531
Palny Shungu <ZLAC276@live.rhul.ac.uk>
2024-02-19 12:50:00 +0000

    Adding registering user form to homepage

--- frontend/frontend-app/src/components/Navbar.js
+++ frontend/frontend-app/src/components/Navbar.js
@@ -6,7 +6,8 @@ const Navbar = () => {
     <div>
       {/* menu */}
       <nav className='justify-content-center'>
-        <a href='/assets'>Home</a> 
+        <a href='/assets'>Home</a>
+        <a href='/register-user'>Register User</a>
         <a href='/create-assets'>Create Assets </a>
         <a href='/create-types'>Create Types</a>
       </nav>
