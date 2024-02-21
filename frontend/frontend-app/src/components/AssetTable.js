f50ccf1d72027c8f572384d417c474a352c63d44
Owen Mashingaidze <zkac157@live.rhul.ac.uk>
2024-02-21 01:54:05 +0000

    Adding connection of frontend to database through 'get-assets' endpoint

--- frontend/frontend-app/src/components/AssetTable.js
+++ frontend/frontend-app/src/components/AssetTable.js
@@ -1,50 +1,50 @@
-import React, { useState, useEffect } from "react";
-import Assets from './assets.json'
-import './AssetTable.css'
-
-function AssetTable(){
-
-    const [assets, setAssets] = useState([]);
-        useEffect(() => {
-            setAssets(Assets);
-    }, []);
-
-    const deleteBtn = (id) => {
-        alert("Are you sure you want to delete this?")
-      };
-   return (
-   <div>
-        <h1> Assets </h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Link</th>
-                    <th>Creator</th>
-                    <th>Date</th>
-                    <th>Type</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                Assets && Assets.map(asset =>(
-                    <tr key={asset.AssetId}>
-                        <td>{asset.Title}</td>
-                        <td>{asset.Link}</td>
-                        <td>{asset.Creator}</td>
-                        <td>{asset.Date}</td>
-                        <td>{asset.Type}</td>
-                        <td>
-                            <button className = "btn btn-danger" 
-                            style = {{marginLeft:"10px"}} onClick={()=>deleteBtn(asset.AssetId)}> Delete</button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-   </div>
-
-  );
-}
-  export default AssetTable;
+import { useState, useEffect } from 'react';
+import "./AssetTable.css"
+const Fetch = () => {
+  const [assets, setAssets] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8080/get-assets')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setAssets(data);
+      });
+  }, []);
+  return (
+    <div>
+         <h1> Assets </h1>
+         <table>
+             <thead>
+                 <tr>
+                     <th>Title</th>
+                     <th>Link</th>
+                     <th>Creator</th>
+                     <th>Date</th>
+                     <th>Type</th>
+                     <th>Actions</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 {
+                 assets.map((asset) =>(
+                     <tr key={asset.asset_id}>
+                         <td>{asset.title}</td>
+                         <td>{asset.link}</td>
+                         <td>{asset.creator}</td>
+                         <td>{asset.creation_date}</td>
+                         <td>{asset.type}</td>
+                         <td>
+                             <button className = "btn btn-danger" 
+                             style = {{marginLeft:"10px"}}> Delete</button>
+                         </td>
+                     </tr>
+                 ))}
+             </tbody>
+         </table>
+    </div>
+ 
+   );
+};
+export default Fetch;
