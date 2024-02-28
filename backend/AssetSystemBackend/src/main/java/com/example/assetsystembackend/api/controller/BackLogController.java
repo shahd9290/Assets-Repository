3b1d50955d848a7d9704fee5b04396d375153acf
Omar <zlac353@live.rhul.ac.uk>
2024-02-28 23:31:57 +0000

    Retrieve the audit trail of a specific asset

--- backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/controller/BackLogController.java
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/controller/BackLogController.java
@@ -1,11 +1,10 @@
 package com.example.assetsystembackend.api.controller;
 
+import com.example.assetsystembackend.api.model.BackLog;
 import com.example.assetsystembackend.api.service.BackLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,16 @@ public class BackLogController {
     @GetMapping("/logs")
     public ResponseEntity<List<String>> getAllLogs() {
         List<String> logs = backLogService.getBackLog();
+        return ResponseEntity.ok(logs);
+    }
+
+    //ID OF THE ASSET
+    @GetMapping("/log{id}")
+    public ResponseEntity<List<String>> getLogById(@PathVariable("id") long id) {
+        List<String> logs = backLogService.getBackLogByAsset(id)
+                .stream()
+                .map(BackLog::getMessage).
+                toList();
         return ResponseEntity.ok(logs);
     }
 
