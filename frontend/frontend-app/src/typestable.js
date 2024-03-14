b3ee3e6889492cd0ac7998ea2348ba561746be98
zlac162 <zlac162@live.rhul.ac.uk>
2024-03-14 19:58:19 +0000

    Delete types

---
+++ frontend/frontend-app/src/typestable.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import "./AssetTable.css"
+const Fetch = () => {
+    const [types, setTypes] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:8080/get-type')
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setTypes(data);
+            });
+    }, []);
+    function deleteBtn(id) {
+        fetch('http://localhost:8080/delete-type', {
+            method: 'DELETE',
+            body: JSON.stringify({ "id": id })
+        })
+            .then((item) => {
+                item.json().then((response) => { console.warn(response) })
+            }
+
+            )
+    }
+    return (
+        <div>
+            <h1> Type </h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Columns</th>
+                        <th>Creator</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        types.map((types) => (
+                            <tr key={types.type_id}>
+                                <td>{types.title}</td>
+                                <td>{types.link}</td>
+                                <td>
+                                    <button className="btn btn-danger"
+                                        style={{ marginLeft: "10px" }} onClick={() => deleteBtn(types.types_id)} > Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+
+    );
+};
+export default Fetch;
