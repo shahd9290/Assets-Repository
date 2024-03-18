ad101b6191f408c04a2d22fa6664391750ee8758
Eman Naveed <zlac358@live.rhul.ac.uk>
2024-03-18 12:28:11 +0000

    endpoint name change

--- backend/AssetSystemBackend/src/test/java/com/example/assetsystembackend/api/controller/TestController.java
+++ backend/AssetSystemBackend/src/test/java/com/example/assetsystembackend/api/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api/test")
 public class TestController {
     @GetMapping("/all")
     public String allAccess(){
