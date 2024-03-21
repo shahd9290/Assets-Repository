335e64084ee7ca8c07e3c89df4150d02a622ceba
Omar <zlac353@live.rhul.ac.uk>
2024-03-21 00:30:18 +0000

    Add Javadocs for backlog Controller & service

--- backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/service/BackLogService.java
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/service/BackLogService.java
@@ -10,39 +10,56 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Service class to manage backlogs.
+ */
 @Service
 public class BackLogService {
 
-
     private final BackLogRepository backLogRepository;
 
-    //Creation message
+    // Creation message
     private final String CREATION_STR = "%s created %s on %s";
 
-    //Deletion message
+    // Deletion message
     private final String DELETION_STR = "%s deleted %s on %s";
-
 
     @Autowired
     public BackLogService(BackLogRepository logRepo) {
         backLogRepository = logRepo;
     }
 
-
+    /**
+     * Get all backlogs.
+     *
+     * @return List of BackLog objects representing backlogs.
+     */
     public List<BackLog> getBackLogs() {
         return backLogRepository.findAll();
     }
 
+    /**
+     * Add a new entry for asset creation to the backlog.
+     *
+     * @param asset The asset that was created.
+     * @return {@code true} if the entry was successfully added to the backlog, {@code false} otherwise.
+     */
     public boolean addAssetCreation(Asset asset) {
         BackLog newEntry = new BackLog();
         newEntry.setAsset(asset.getId());
 
-        //Generate message
-        newEntry.setMessage(String.format(CREATION_STR,asset. getCreatorName(), asset.getName(), asset.getCreationDate().toString()));
+        // Generate message
+        newEntry.setMessage(String.format(CREATION_STR, asset.getCreatorName(), asset.getName(), asset.getCreationDate().toString()));
         BackLog returned = backLogRepository.save(newEntry);
         return returned.getId() != null;
     }
 
+    /**
+     * Add a new entry for asset deletion to the backlog.
+     *
+     * @param asset The asset that was deleted.
+     * @return {@code true} if the entry was successfully added to the backlog, {@code false} otherwise.
+     */
     public boolean addAssetDeletion(Asset asset) {
         BackLog newEntry = new BackLog();
         newEntry.setAsset(asset.getId());
@@ -51,18 +68,19 @@ public class BackLogService {
         LocalDate currentDate = LocalDate.now();
         Date date = Date.valueOf(currentDate);
 
-        //Generate message
-        newEntry.setMessage(String.format(DELETION_STR,asset. getCreatorName(), asset.getName(), date));
+        // Generate message
+        newEntry.setMessage(String.format(DELETION_STR, asset.getCreatorName(), asset.getName(), date));
         BackLog returned = backLogRepository.save(newEntry);
         return returned.getId() != null;
     }
 
-
+    /**
+     * Get backlogs associated with a specific asset.
+     *
+     * @param id The ID of the asset.
+     * @return List of BackLog objects representing backlogs associated with the asset.
+     */
     public List<BackLog> getBackLogByAsset(Long id) {
         return backLogRepository.findByAssetId(id);
-
     }
-
-
-
 }
