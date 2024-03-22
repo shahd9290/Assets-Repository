340436f53922cc3b22dc39090eb014432b777971
Owen Mashingaidze <zkac157@live.rhul.ac.uk>
2024-03-22 00:36:35 +0000

    adding functionality to the login page

--- frontend/frontend-app/src/UserLogin.js
+++ frontend/frontend-app/src/UserLogin.js
@@ -1,6 +1,27 @@
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function UserLogin() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function onSubmit(){
+    const payload = {
+      "username": username,
+      "password": password
+    }
+    try{
+      const loggedIn = await axios.post('http://localhost:8080/api/auth/signin',payload);
+      console.log("log in successful");
+      alert("Login successful!");
+    }catch (error){
+      const errorMessage = error.response ? error.response.data : error.message;
+      console.error(errorMessage);
+      alert(errorMessage)
+    }
+  }
   return (
     <div className="UserLogin" style={{ marginLeft: '15%', padding: '1px 16px', height: '1000px' }}>
       <head>
@@ -10,13 +31,13 @@ function UserLogin() {
         <form>
           <label for="userName">Enter your username:</label>
           <p></p>
-          <input type="text" id="userName" name="user" required/>
+          <input type="text" id="userName" name="user" value={username} onChange={event => setUsername(event.target.value)}required/>
           <p></p>
           <label for="passWord">Enter your password:</label>
           <p></p>
-          <input type="password" id="passWord" name="pass" required/>
+          <input type="password" id="passWord" name="pass" value={password} onChange={event=> setPassword(event.target.value)}required/>
           <p></p>
-          <button type="submit">Register User</button>
+          <button type="submit" onSubmit={onSubmit}>Login</button>
         </form>
     </div>
   );
