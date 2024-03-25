ae8721d8ee49dd9a95adaa447749cce871e4e030
Danyal Shah <zlac208@live.rhul.ac.uk>
2024-03-25 13:44:06 +0000

    Add Javadoc for AuthController. Refactoring.

--- backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/controller/AuthController.java
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/controller/AuthController.java
@@ -29,6 +29,9 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Controller used to manage users, including creation and authentication.
+ */
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("api/auth")
@@ -49,39 +52,56 @@ public class AuthController {
     @Autowired
     JwtUtils jwtUtils;
 
+    /**
+     * Authenticates a user by validating the provided login credentials and generates a JWT token upon successful authentication.
+     *
+     * @param loginRequest The login request containing the username and password to authenticate.
+     * @return A summary of the user details, including the token used throughout the system.
+     */
     @PostMapping("/signin")
-            public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        // Use credentials to authenticate
         Authentication authentication = authManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
                         loginRequest.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = "hello";
+        // Initialize JWT token;
+        String jwt = "";
+        // Generate token.
         if (authentication.isAuthenticated()) {
-             jwt = jwtUtils.generateJwtToken(authentication);
-        }else {
+            jwt = jwtUtils.generateJwtToken(authentication);
+        } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
+        // Extract user details.
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         List<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority)
                 .toList();
         String role;
-        //if (roles.contains(ERole.ROLE_ADMIN.toString())){
         if (Objects.equals(roles.get(0), ERole.ROLE_ADMIN.toString())) {
             role = "admin";
         } else if (Objects.equals(roles.get(0), ERole.ROLE_USER.toString())) {
             role = "user";
-        }else{
+        } else {
             role = "viewer";
         }
+        // Output
         JwtResponse test = new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(),
                 userDetails.getEmail(), role);
 
         return ResponseEntity.ok(test);
     }
 
+    /**
+     * Registers a new user with the provided signup information.
+     *
+     * @param signupRequest The signup request containing user details such as username, email, and password.
+     * @return A response dependent on whether the user has been created, already exists or details are missing.
+     */
     @PostMapping("/signup")
-            public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest){
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        // Check if email and username are already taken.
         if (userRepo.existsByUsername((signupRequest.getUsername()))) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
         }
@@ -94,6 +114,8 @@ public class AuthController {
         Set<String> strRoles = signupRequest.getRole();
         Set<Role> roles = new HashSet<>();
 
+        // Check if roles are valid. If none provided then return exception.
+        // Else assign the corresponding role to the user.
         if (strRoles == null) {
             Role viewerRole = roleRepo.findByName(ERole.ROLE_VIEWER)
                     .orElseThrow(() -> new RuntimeException("Error: Role not found."));
