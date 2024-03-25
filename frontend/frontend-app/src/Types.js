dafc4d421731b49104e53c0d749c252c1ac73555
zlac162 <zlac162@live.rhul.ac.uk>
2024-03-25 16:47:28 +0000

    Add Types working (bugs fixed)

--- frontend/frontend-app/src/Types.js
+++ frontend/frontend-app/src/Types.js
@@ -1,30 +1,76 @@
 import './App.css';
 
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
 
 function Types() {
+  const [formData, setFormData] = useState({
+    table_name: '',
+    columns: [],
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const payload = {
+        table_name: formData.table_name,
+        columns: formData.columns.split(",")
+      };
+      const response = await axios.post(`http://localhost:8080/type/add-type`, payload);
+      console.log(response.data);
+      alert("Type created successfully!");
+      setFormData({
+        table_name: '',
+        columns: []
+      });
+
+
+    } catch (error) {
+      // Error handling
+      const errorMessage = error.response ? error.response.data : error.message;
+      console.error('Error submitting form:', errorMessage);
+      alert("Failed to create type. Error: " + (typeof errorMessage === 'string' ? errorMessage : JSON.stringify(errorMessage, null, 2)));
+    }
+  };
+
+
   return (
     <div className="App">
-      <head>
-        <title>Type Management System</title>
-      </head>
-      <body>
-        <h1>Create New Type</h1>
-        <form>
-          <label for="type-title">Title:</label>
-          <p></p>
-          <input type="text" id="type-title" name="title" required/>
-          <p></p>
-          <label for="type-description">Description:</label>
-          <p></p>
-          <textarea id="type-description" name="description" required></textarea>
-          <p></p>
-          <button id="createButton" onclick="handleButtonClick(id)" type="submit">Create Type</button>
-        </form>
-        <button id="deleteButton" onclick="handleButtonClick(id)">Delete Type</button>
-      </body>
+      <h1>Create New Type</h1>
+      <form onSubmit={handleSubmit}> {/* Ensure handleSubmit is defined */}
+        <label htmlFor="table_name">Type:</label>
+        <textarea
+          id="table_name"
+          name="table_name"
+          required
+          value={formData.table_name}
+          onChange={handleChange}
+        />
+        <label htmlFor="type-columns">Column (Seperated by commas):</label>
+        <textarea
+          id="type-columns"
+          name="columns"
+          required
+          value={formData.columns}
+          onChange={handleChange}
+        />
+        <button type="submit">Create type</button>
+      </form>
     </div>
   );
 }
+
 
 
 export default Types;
