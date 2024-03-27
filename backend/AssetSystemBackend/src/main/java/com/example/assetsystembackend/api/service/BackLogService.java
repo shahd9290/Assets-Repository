172017cc6f0033eb2e96df5d5b6801b21d65dc68
Omar <zlac353@live.rhul.ac.uk>
2024-03-27 23:23:13 +0000

    Create backlog entry for asset edit

--- backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/service/BackLogService.java
+++ backend/AssetSystemBackend/src/main/java/com/example/assetsystembackend/api/service/BackLogService.java
@@ -6,6 +6,8 @@ import com.example.assetsystembackend.api.repository.BackLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,6 +22,8 @@ public class BackLogService {
 
     //Creation message
     private final String CREATION_STR = "%s created %s on %s";
+
+    private final String EDIT_ASSET_STR = "%s edited %s on %s";
 
 
     @Autowired
@@ -37,7 +41,7 @@ public class BackLogService {
         newEntry.setAsset(asset.getId());
 
         //Generate message
-        newEntry.setMessage(String.format(CREATION_STR,asset. getCreatorName(), asset.getName(), asset.getCreationDate().toString()));
+        newEntry.setMessage(String.format(CREATION_STR, asset.getCreatorName(), asset.getName(), asset.getCreationDate().toString()));
         BackLog returned = backLogRepository.save(newEntry);
         return returned.getId() != null;
     }
@@ -45,6 +49,19 @@ public class BackLogService {
     public List<BackLog> getBackLogByAsset(Long id) {
         return backLogRepository.findByAssetId(id);
 
+    }
+
+    public boolean addAssetEdit(Asset asset) {
+        BackLog newEntry = new BackLog();
+        newEntry.setAsset(asset.getId());
+
+        // Generate message with today's date
+        LocalDate today = LocalDate.now();
+        String formattedDate = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")); // You can adjust the date format as needed
+        newEntry.setMessage(String.format(EDIT_ASSET_STR, asset.getCreatorName(), asset.getName(), formattedDate));
+
+        BackLog returned = backLogRepository.save(newEntry);
+        return returned.getId() != null;
     }
 
 
